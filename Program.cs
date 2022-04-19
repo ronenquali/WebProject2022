e5b8a8e5873307d5d2a893005704e9d7ba8c11ab
ronenquali <ronen.a@quali.com>
2022-04-19 16:21:16 +0300

    allow getting time

--- Program.cs
+++ Program.cs
@@ -2,5 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Goodbye World!!!");
+app.MapGet("/time", () => DateTime.Now.ToLongTimeString());
 
 app.Run();
