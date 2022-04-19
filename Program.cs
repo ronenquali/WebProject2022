aa5b3afc780393d293702fa58cae42afbc368e0b
ronenquali <ronen.a@quali.com>
2022-04-19 16:23:44 +0300

    Allow getting date

--- Program.cs
+++ Program.cs
@@ -2,5 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Goodbye World!!!");
+app.MapGet("/date", () => DateTime.Now.ToLongDateString());
 
 app.Run();
